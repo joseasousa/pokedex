7d90670992ca13cfcd970c41e5dbd03df02e76a7
jose alves sousa neto <a.sousajose@gmail.com>
2018-02-28 15:04:02 -0400

    obtendo pokemons

--- src/store/sagas/pokemon.js
+++ src/store/sagas/pokemon.js
@@ -8,8 +8,21 @@ export function * getPokemons () {
   const response = yield call(api.get, '/pokemon')
 
   if (response.ok) {
-    yield put(ActionCreators.pokemonSuccess(response.data))
+    const pokemons = response.data.results.map(async item =>
+      getIndividualPoke(item.url))
+
+    const resul = yield Promise.all(pokemons)
+    yield put(ActionCreators.pokemonSuccess(resul))
   } else {
     yield put(ActionCreators.pokemonFailure())
+  }
+}
+
+const getIndividualPoke = async url => {
+  const pokemon = await api.get(url)
+  if (pokemon.ok) {
+    return pokemon.data
+  } else {
+    return pokemon.problem
   }
 }
