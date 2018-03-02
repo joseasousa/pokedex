1efcc62b558fe008c2cf3be8feb1149437cb8289
jose alves sousa neto <a.sousajose@gmail.com>
2018-03-02 09:15:41 -0400

    testeCD

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 
 import { create } from 'apisauce'
 
-//const baseURL = 'http://localhost/api/v2/'
-const baseURL = 'https://pokeapi.co/api/v2/'
+const baseURL = 'http://localhost/api/v2/'
+//const baseURL = 'https://pokeapi.co/api/v2/'
 
 const api = create({
   baseURL
